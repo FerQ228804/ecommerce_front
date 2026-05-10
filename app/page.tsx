9037b50457c83adf6d2a5aa43461200ce1267f35
Fernando Quimbayo <fernandoquimbayo@hotmail.com>
2026-05-10 12:56:38 -0500

    component inclusion, add library shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
 
 export default function Home() {
   return (
